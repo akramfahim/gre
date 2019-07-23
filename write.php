c67a05960b34d0f19a93cef25b3f1b77730b6e33
Akram Fahim <akram.fahim31@gmail.com>
2019-07-23 20:07:14 +0600

    Adding Write.php Page

---
+++ write.php
@@ -0,0 +1,14 @@
+<?php 
+	require 'header.php';
+	require 'navbar.php';
+ ?>
+<div class="container">
+	<div class="row">
+		<div class="col-md-4 offset-md-4">
+			<h1 class="text-center text-success">
+				Hello Write Page
+			</h1>
+		</div>
+	</div>
+</div>
+ <?php require 'footer.php'; ?>
