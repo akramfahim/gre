b68f87b688e199fc6f9692330e622bf011a62095
Akram Fahim <akram.fahim31@gmail.com>
2019-08-01 01:37:41 +0600

    Create main.js
    try some js code

---
+++ js/main.js
@@ -0,0 +1,9 @@
+/*$(document).ready(function(){
+ $(".form input[type=radio]").each(function () {
+	$(this).prop('checked', false);
+   });
+});*/
+
+	/*if ( window.history.replaceState ) {
+        window.history.replaceState( null, null, window.location.href );
+    }*/
