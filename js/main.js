b3f71291a0568ddb3b4105f8cfd4711515406786
Akram Fahim <akram.fahim31@gmail.com>
2019-08-01 16:46:24 +0600

    Update main.js
    add some code

--- js/main.js
+++ js/main.js
@@ -4,6 +4,6 @@
    });
 });*/
 
-	/*if ( window.history.replaceState ) {
-        window.history.replaceState( null, null, window.location.href );
-    }*/
+if ( window.history.replaceState ) {
+    window.history.replaceState( null, null, window.location.href );
+}
