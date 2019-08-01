52542f2b1899461f960d601f69f3e4f6e9204ce1
Akram Fahim <akram.fahim31@gmail.com>
2019-08-01 21:01:24 +0600

    Update barron_level_two.php
    Level two form action page

--- barron_level_two.php
+++ barron_level_two.php
@@ -12,29 +12,6 @@
         $level_two_correct_ans_two = '';
         $level_two_correct_ans_three = '';
 
-        /*$level_two_statementOne = $pdo->prepare("SELECT * FROM level_two_question_barron ORDER BY RAND() limit 3");
-        $level_two_statementOne->execute();
-        $level_two_resultOne = $level_two_statementOne->fetchAll(PDO::FETCH_ASSOC);*/
-
-        /* Get Random First Question */
-        /*$level_two_ques_id = $level_two_resultOne[0]['id'];
-        $level_two_random_question =  $level_two_resultOne[0]['question'];
-        $level_two_correct_ans = $level_two_resultOne[0]['answer'];*/
-
-        /* Get Random Second Question */
-        /*$level_two_ques_id_two = $level_two_resultOne[1]['id'];
-        $level_two_random_question_two =  $level_two_resultOne[1]['question'];
-        $level_two_correct_ans_two = $level_two_resultOne[1]['answer'];*/
-
-        /* Get Random Third Question */
-        /*$level_two_ques_id_three = $level_two_resultOne[2]['id'];
-        $level_two_random_question_three =  $level_two_resultOne[2]['question'];
-        $level_two_correct_ans_three = $level_two_resultOne[2]['answer'];*/
-
-        /*echo $level_two_correct_ans."<br>";
-        echo $level_two_correct_ans_two."<br>";
-        echo $level_two_correct_ans_three."<br><hr>";*/
-
         /* Level 2 Test */
         if (isset($_POST['level_two_submit'])) {
         	if(empty($_POST['level_two_answer']) || empty($_POST['level_two_answer2']) || empty($_POST['level_two_answer3']) ) {
@@ -48,13 +25,24 @@
         		$level_two_ques_id_two = $_POST['level_two_ques_id_two'];
         		$level_two_ques_id_three = $_POST['level_two_ques_id_three'];
 
-        		$level_two_sql_one = $pdo->prepare("SELECT answer FROM level_two_question_barron WHERE question_id=?");
-        		$level_two_sql_one->execute(array($level_two_ques_id));
-        		$level_two_correct_ans = $level_two_sql_one->fetch();
-        		$corr_ans_one = $level_two_correct_ans['answer'];
 
-        		echo $corr_ans_one;;
+                /* Getting Correct Answer related to the question */
+                $stmt_one = $pdo->prepare("SELECT answer FROM level_two_question_barron WHERE id=?");
+                $stmt_one->execute(array($level_two_ques_id));
+                $resultOne = $stmt_one->fetch();
+                $level_two_correct_ans = $resultOne['answer'];
 
+                $stmt_two = $pdo->prepare("SELECT answer FROM level_two_question_barron WHERE id=?");
+                $stmt_two->execute(array($level_two_ques_id_two));
+                $resultTwo = $stmt_two->fetch();
+                $level_two_correct_ans_two = $resultTwo['answer'];
+
+                $stmt_three = $pdo->prepare("SELECT answer FROM level_two_question_barron WHERE id=?");
+                $stmt_three->execute(array($level_two_ques_id_three));
+                $resultThree = $stmt_three->fetch();
+                $level_two_correct_ans_three = $resultThree['answer'];
+
+                
 
                 $level_two_answer = $_POST['level_two_answer'];
                 $level_two_answer2 = $_POST['level_two_answer2'];
@@ -66,8 +54,8 @@
 
         		if (
                 	$level_two_answer  == $level_two_correct_ans  
-                	|| $level_two_answer2 == $level_two_correct_ans_two 
-                	|| $level_two_answer3 == $level_two_correct_ans_three
+                	&& $level_two_answer2 == $level_two_correct_ans_two 
+                	&& $level_two_answer3 == $level_two_correct_ans_three
                 ) {
 
                     $level_two_sql1 = $pdo->prepare("SELECT * FROM barron_word_settings WHERE user_id=?");
@@ -75,27 +63,27 @@
                     $level_two_totalrow = $level_two_sql1->rowCount();              
                     if($level_two_totalrow) {
 
-                    	/*$level_two_sql = $pdo->prepare("UPDATE barron_word_settings SET levelTwo=? WHERE user_id= ?");
+                    	$level_two_sql = $pdo->prepare("UPDATE barron_word_settings SET levelTwo=? WHERE user_id= ?");
                         $level_two_sql->execute(array('Completed',$_POST['user_id']));
                        
                         $_SESSION['success_message'] = 'You Passed Level Two Now!!';
-                  		header('Location:single_course.php');*/
+                  		header('Location:single_course.php');
 
-                  		echo "You Passed";
+                  		/*echo "You Passed";*/
 
                     }else{
                     
-                    	/*$_SESSION['error_message'] = 'You Have not Completed Level One Yet';
-                  		header('Location:single_course.php');*/
-                  		echo "You Failed level 1";
+                    	$_SESSION['error_message'] = 'You Have not Completed Level One Yet';
+                  		header('Location:single_course.php');
+                  		/*echo "You Failed level 1";*/
 
                     }
                     // unset($_POST['answer']);
                   }else{
 
-                    /*$_SESSION['error_message'] = 'Your Level Two Answer is not correct';
-                  	header('Location:single_course.php');*/
-                  	echo "Your Answers are wrong";
+                    $_SESSION['error_message'] = 'Your Level Two Answers are not correct';
+                  	header('Location:single_course.php');
+                  	/*echo "Your Answers are wrong";*/
                     // unset($_POST['answer']);
                 }
             }        
