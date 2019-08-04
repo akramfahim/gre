5adc1b2c9468fd35cda590fc1267aaf052e6ca5e
Akram Fahim <akram.fahim31@gmail.com>
2019-08-04 23:27:16 +0600

    Update test.php
    Test page for all users

--- test.php
+++ test.php
@@ -1,13 +1,13 @@
 <?php
    include "header.php";
 
-      ob_start();
+     ob_start();
      session_start();
      include("inc/config.php");
      include("inc/CSRF_Protect.php");
      $csrf = new CSRF_Protect();
-     $error_msg = '';
-     $succss_msg = '';
+     $error_message = '';
+     $success_message = '';
 
      /* Level One Question Barron*/
     $ques_id='';
@@ -20,29 +20,11 @@
     /* Get Random First Question */
     $ques_id = $resultOne[0]['id'];
     $random_question =  $resultOne[0]['question'];
-    $correct_ans = $resultOne[0]['answer'];
 
 
     $ModalQuestionOne = $pdo->prepare("SELECT * FROM level_one_question_option where level_one_question_option.question_id = ".$ques_id." limit 4");
     $ModalQuestionOne->execute();
     $resultModalOne = $ModalQuestionOne->fetchAll(PDO::FETCH_ASSOC); 
-
-    if (isset($_POST['test_submit'])) {
-    	$answer = $_POST['answer'];
-
-    	echo $correct_ans."<br>";
-    	echo $answer."<br>";
-
-    	if ($answer == $correct_ans) {
-    		$succss_msg = "Your Answer is Correct";
-    	}else{
-    		$error_msg = "Oppps! Your Answer is not Correct";
-    	}
-    }
-
-    
-
-
 
 
    require 'navbar.php'; 
@@ -50,25 +32,25 @@
 
 <div class="container">
 	<div class="row">
-		<div class="col-md-6 col-sm-12 offset-md-3">
-			<?php if ($error_msg): ?>
-				<div class="alert alert-danger">
-					<h3><?php echo $error_msg; ?></h3>
-				</div>
-			<?php endif ?>
+		<div class="col-md-6 col-sm-12 offset-md-3 align-self-center">
+		<?php 
+            if (!empty($_SESSION['success_message'])) {
+                echo '<div class="alert alert-success"><h3 class="text-center"> '.$_SESSION['success_message'].'</h3></div>';
+                unset($_SESSION['success_message']);
+            }
 
-			<?php if ($succss_msg): ?>
-				<div class="alert alert-success">
-					<h3><?php echo $succss_msg; ?></h3>
-				</div>
-			<?php endif ?>
+            if (!empty($_SESSION['error_message'])) {
+                echo '<div class="alert alert-danger"><h3 class="text-center"> '.$_SESSION['error_message'].'</h3></div>';
+                unset($_SESSION['error_message']);
+            }
+        ?>
 			
-			<div class="card my-5">
+			<div class="card my-3">
 			    <div class="card-header">
 			    	<h2 class="text-center">Test Your Vocabs Range</h2>
 				</div>
 			    <div class="card-body">
-			    	<form action="" method="post">
+			    	<form action="form/test_action.php" method="post">
 						
 						<h5>Q: <b> <?php echo $random_question;  ?> ?</b></h5>
         				<hr>
