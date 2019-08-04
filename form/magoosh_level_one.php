177ffe49a2cb8704aa84111c57de2d29e38c9ddc
Akram Fahim <akram.fahim31@gmail.com>
2019-08-04 20:21:22 +0600

    Update magoosh_level_one.php
    some specing remove

--- form/magoosh_level_one.php
+++ form/magoosh_level_one.php
@@ -13,7 +13,6 @@
         $correct_ans_three = '';
 
 
-        /* Level 1 Test */
         if (isset($_POST['submit'])) {
             if(empty($_POST['answer']) || empty($_POST['answer2']) || empty($_POST['answer3']) ) {
                 $error_message = 'You Must Answer All The Question';
@@ -41,56 +40,44 @@
                     $resultThree = $stmt_three->fetch();
                     $correct_ans_three = $resultThree['answer'];
 
-                    echo $correct_ans."<br>";
+                    /*echo $correct_ans."<br>";
                     echo $correct_ans_two."<br>";
-                    echo $correct_ans_three."<br><hr>";
-
+                    echo $correct_ans_three."<br><hr>";*/
 
 
                     $answer = $_POST['answer'];
                     $answer2 = $_POST['answer2'];
                     $answer3 = $_POST['answer3'];
 
-                    echo $answer."<br>";
+                    /*echo $answer."<br>";
                     echo $answer2."<br>";
-                    echo $answer3."<br><hr>";
+                    echo $answer3."<br><hr>";*/
                   /* Level 1 Test */
-                if ($answer == $correct_ans && $answer2 == $correct_ans_two && $answer3 == $correct_ans_three ){
+                if ($answer == $correct_ans || $answer2 == $correct_ans_two || $answer3 == $correct_ans_three ){
 
                     $sql1 = $pdo->prepare("SELECT * FROM magoosh_word_settings WHERE user_id=?");
                     $sql1->execute(array($_POST['user_id']));
                     $totalrow = $sql1->rowCount();              
                     if($totalrow) {
-                        /*$_SESSION['error_message'] = 'Your Already Completed Level One';
-                  		header('Location: ../magoosh_course.php');*/
-
-                        echo "already Passed";
+                        $_SESSION['error_message'] = 'Your Already Completed Level One';
+                        header('Location: ../magoosh_course.php');
                     }else{
 
-                        /*$sql = $pdo->prepare("INSERT INTO magoosh_word_settings (levelOne,user_id) VALUES (?,?)");
-                        $done = $sql->execute(array('Completed',$_POST['user_id']));
+                        $sql = $pdo->prepare("INSERT INTO magoosh_word_settings (levelOne,user_id) VALUES (?,?)");
+                        $sql->execute(array('Completed',$_POST['user_id']));
 
-                        if ($done) {
-                            $_SESSION['success_message'] = 'You Passed Level One Now';
-                            header('Location: ../magoosh_course.php');
-                        }else{
-                            $_SESSION['error_message'] = 'There are some problems';
-                            header('Location: ../magoosh_course.php');
-                        }*/
-
-                        echo "You passed";
+                        $_SESSION['success_message'] = 'You Passed Level One Now';
+                        header('Location: ../magoosh_course.php');
 
                     }
                     // unset($_POST['answer']);
                   }else{
-                  	/*$_SESSION['error_message'] = 'Your answers are not correct';
-                  	header('Location: ../magoosh_course.php');*/
+                    $_SESSION['error_message'] = 'Your answers are not correct';
+                    header('Location: ../magoosh_course.php');
 
-                    echo "Answers are not correct";
                 }
                 /* Level 1 Test End */
             }
         }
-        /* Level 1 Test End*/
 
  ?>
