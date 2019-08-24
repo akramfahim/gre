7179d61172c0128dd002b7b25894f554eed60d0b
Akram Fahim <akram.fahim31@gmail.com>
2019-08-24 16:22:22 +0600

    Update fill.php
    100% ok FIle

--- fill.php
+++ fill.php
@@ -31,28 +31,42 @@
 		<div class="card-body">
 			 <?php
 
-			      $level_sql = $pdo->prepare("SELECT * FROM settings WHERE user_id=?");
-		          $level_sql->execute(array($user_id));
-		          $level_pass = $level_sql->fetchAll(PDO::FETCH_ASSOC);
-		          
-		          if ($level_pass[0]['manhattan'] == 'Completed') {
-  	                  $statement = $pdo->prepare("SELECT * FROM `word_table` ORDER BY RAND() limit 5");
-		          }
-		          else if ($level_pass[0]['magoosh'] == 'Completed'){
-		          	 $statement = $pdo->prepare("SELECT * FROM `word_table` where type='barron' OR type='magoosh' ORDER BY RAND() limit 5");
-		          }
-		           else if ($level_pass[0]['barron'] == 'Completed'){
-		          	 $statement = $pdo->prepare("SELECT * FROM `word_table` where type='barron' ORDER BY RAND() limit 5");
-		          }
+             $level_sql = $pdo->prepare("SELECT * FROM settings WHERE user_id=?");
+              $level_sql->execute(array($user_id));
+              $level_pass = $level_sql->fetchAll(PDO::FETCH_ASSOC);
+              
+              if (empty($level_pass)) {
+                $err_msg = "To Give the Test You Have to Completed Barron Level Atleast";
+              }
+              else if ($level_pass[0]['manhattan'] == 'Completed') {
 
-                  $statement->execute();
+                      $statement = $pdo->prepare("SELECT * FROM `question` ORDER BY RAND() limit 5");
+                      $statement->execute();
+                      $results = $statement->fetchAll(PDO::FETCH_ASSOC);
+              }
+              else if ($level_pass[0]['magoosh'] == 'Completed'){
+
+                 $statement = $pdo->prepare("SELECT * FROM `question` where type='barron' OR type='magoosh' ORDER BY RAND() limit 5");
+                 $statement->execute();
                   $results = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-                  	
-                  
-                  ?>
-			
-             <form action="form/fill.php" method="post">
+              }
+              else if ($level_pass[0]['barron'] == 'Completed'){
+
+                 $statement = $pdo->prepare("SELECT * FROM `question` where type='barron' ORDER BY RAND() limit 5");
+                 $statement->execute();
+                $results = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+              }
+
+        ?> 
+
+        <?php if ($err_msg): ?>
+              <div class="alert alert-danger">
+                <h3><?php echo $err_msg; ?></h3>
+              </div>
+            <?php else: ?>
+              <form action="form/fill.php" method="post">
                <input type="hidden" name="user_id" value="<?php echo $_SESSION['user']['id']; ?>">
                <?php foreach ($results as $key => $row3) { ?>
 
@@ -73,6 +87,10 @@
                <hr/>
                <?php } ?>
                  <button class="btn btn-success btn-block" type="submit" name="submit"> Submit </button>
+              </form>
+            <?php endif ?>
+			
+             
 
 		</div>
 	</div>
