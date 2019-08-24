323675a0c40101cbab2f635acd433bb81884f581
Akram Fahim <akram.fahim31@gmail.com>
2019-08-24 13:36:54 +0600

    Update word_edit.php
    Update word and Description

--- admin/word_edit.php
+++ admin/word_edit.php
@@ -54,7 +54,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <title>Login | Admin Panel</title>
+  <title>Dashboard | Admin Panel</title>
 
   <link rel="stylesheet" href="../css/bootstrap.min.css">
 </head>
